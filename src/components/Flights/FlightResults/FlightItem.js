7556c4a0462172f6db8cb12e18c5dd33af7617fd
goskam <matusiak.goska@gmail.com>
2023-06-12 23:27:21 +0200

    Fixed flight item

---
+++ src/components/Flights/FlightResults/FlightItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Card from '../../UI/Card'
+
+const FlightItem = (props) => {
+
+    return (<Card>
+        <p>Flight 1</p>
+    </Card>)
+}
+
+export default FlightItem;
