945de941c0655018094e34b164bc2664d70c4a7f
goskam <matusiak.goska@gmail.com>
2023-06-13 10:40:52 +0200

    Display flight details in FlightItem

--- src/components/Flights/FlightResults/FlightItem.js
+++ src/components/Flights/FlightResults/FlightItem.js
@@ -4,7 +4,12 @@ import Card from '../../UI/Card'
 const FlightItem = (props) => {
 
     return (<Card>
-        <p>Flight 1</p>
+        <p>Airline: {props.airlines}</p>
+       
+            <h2>Departure time: {props.depTime}</h2>
+            <h2>Arrival time: {props.arrTime}</h2>
+            <p>Price: {props.price} PLN</p>
+     
     </Card>)
 }
 
