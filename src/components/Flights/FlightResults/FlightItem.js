93ec64399286760585fc071ea7aaeb88a6ecb9ce
goskam <matusiak.goska@gmail.com>
2023-06-13 11:18:37 +0200

    Display dep, arr using FlightTime component

--- src/components/Flights/FlightResults/FlightItem.js
+++ src/components/Flights/FlightResults/FlightItem.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import Card from '../../UI/Card'
+import FlightTime from './FlightTime'
 
 const FlightItem = (props) => {
 
     return (<Card>
         <p>Airline: {props.airlines}</p>
        
-            <h2>Departure time: {props.depTime}</h2>
-            <h2>Arrival time: {props.arrTime}</h2>
+            <h2>Departure time:</h2>
+            <FlightTime dateTime={props.depTime}/>
+
+            <h2>Arrival time:</h2>
+            <FlightTime dateTime={props.arrTime}/>
+
             <p>Price: {props.price} PLN</p>
+
      
     </Card>)
 }
