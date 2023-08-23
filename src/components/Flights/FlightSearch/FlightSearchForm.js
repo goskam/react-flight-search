ac422ee4200046ef404fe73a8eb0cc6a7b422cf2
goskam <matusiak.goska@gmail.com>
2023-08-23 10:59:18 +0200

    Removed form, added flightSearchHandler to Button

--- src/components/Flights/FlightSearch/FlightSearchForm.js
+++ src/components/Flights/FlightSearch/FlightSearchForm.js
@@ -2,63 +2,19 @@ import React, {useState} from 'react'
 import Card from '../../UI/Card'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { DatePicker } from '@mui/x-date-pickers';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-
-
-function searchFlight() {
-
-    const options = {
-      method: 'GET',
-      headers: {
-        "accept": "application/json",
-         "apikey": "5xPTuuSyI7SiahzNlM4oAKKQJW8Ji731"
-
-      }
-    };
-
-    let flyFrom = "NRN"
-    let flyTo = "PMI"
-    let dateFrom = "27%2F06%2F2023"
-    let dateTo = "27%2F06%2F2023"
-    let curr = "PLN"
-    let direct = 0
-    let adults = 1
-
-    
-    fetch("https://api.tequila.kiwi.com/v2/search?"+ 
-    "fly_from=" + flyFrom + 
-    "&fly_to=" + flyTo + 
-    "&date_from="+ dateFrom + 
-    "&date_to=" + dateTo +
-    "&curr=" + curr +
-    "&max_stopovers=" + direct +
-    "&adults=" + adults
-    //+ "&nights_in_dst_from=2&nights_in_dst_to=3&max_fly_duration=20&flight_type=round&ret_from_diff_city=true&ret_to_diff_city=true&one_for_city=0&one_per_date=0&adults=2&children=2&selected_cabins=C&mix_with_cabins=M&adult_hold_bag=1%2C0&adult_hand_bag=1%2C1&child_hold_bag=2%2C1&child_hand_bag=1%2C1&only_working_days=false&only_weekends=false&partner_market=us&max_stopovers=2&max_sector_stopovers=2&vehicle_type=aircraft&limit=500"
-    , options)
-      .then(response => response.json())
-      .then(response => {
-        console.log(response)
-
-        for (let i = 0; i < response.data.length; i++)  {
-
-            console.log("DEP: " + response.data[i].local_departure + ", ARR: " + response.data[i].local_arrival + ", PRICE: " + response.data[i].price + " " + curr)
-
-		}
-
-
-      })
-      .catch(error => console.log('error', error));
-
-}
-
+//import { DatePicker } from '@mui/x-date-pickers';
+//import { LocalizationProvider } from '@mui/x-date-pickers';
+//import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 const FlightSearchForm = (props) => {
+//test comment
+    const [enteredDepartureAirport, setDepartureAirport] = useState('WAW');
+    const [enteredArrivalAirport, setArrivalAirport] = useState('PMI');
+    const [selectedDepartureDateFrom, setSelectedDepartureDateFrom] = useState('')
+    const [selectedDepartureDateTo, setSelectedDepartureDateTo] = useState('')
+    const [selectedReturnDateFrom, setSelectedReturnDateFrom] = useState('')
+    const [selectedReturnDateTo, setSelectedReturnDateTo] = useState('')
 
-    const [enteredDepartureAirport, setDepartureAirport] = useState('Warszawa');
-    const [enteredArrivalAirport, setArrivalAirport] = useState('Poznan');
-    const [selectedDepartureDate, setSelectedDepartureDate] = useState('')
 
     const DEPAirportChangeHandler = (event) => {
         setDepartureAirport(event.target.value);
@@ -68,8 +24,20 @@ const FlightSearchForm = (props) => {
         setArrivalAirport(event.target.value);
     }
 
-    const DEPDateChangeHandler = (event) => {
-        setSelectedDepartureDate(event.target.value);
+    const DEPDateChangeHandlerFrom = (event) => {
+        setSelectedDepartureDateFrom(event.target.value);
+    }
+
+    const DEPDateChangeHandlerTo = (event) => {
+        setSelectedReturnDateFrom(event.target.value);
+    }
+
+    const RETDateChangeHandlerFrom = (event) => {
+        setSelectedReturnDateTo(event.target.value);
+    }
+
+    const RETDateChangeHandlerTo = (event) => {
+        setSelectedDepartureDateTo(event.target.value);
     }
 
     const flightSearchHandler = (event) => {
@@ -79,7 +47,9 @@ const FlightSearchForm = (props) => {
         const params = {
             departureAirport: enteredDepartureAirport,
             arrivalAirport: enteredArrivalAirport,
-            departureDate: selectedDepartureDate
+            departureDateFrom: selectedDepartureDateFrom,
+            departureDateTo: selectedDepartureDateTo
+
         }
 
         props.onSearchFlights(params);
@@ -87,14 +57,20 @@ const FlightSearchForm = (props) => {
     }
 
     return (<Card>
-        <form onSubmit={flightSearchHandler} display="flex">        
+
+            <div display="flex"> 
             <TextField id="fly_from" label="From" variant="outlined" value={enteredDepartureAirport} onChange={DEPAirportChangeHandler}/>
             <TextField id="fly_to" label="To" variant="outlined" value={enteredArrivalAirport} onChange={ARRAirportChangeHandler}/>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker id="departure_date" label="Date" value={selectedDepartureDate} onChange={DEPDateChangeHandler} />
-            </LocalizationProvider>
-            <Button type="submit" variant="contained" onClick={searchFlight}>Search</Button>
-        </form>
+            </div>  
+            One way, departure between:
+            <div display="flex">
+            <input type="date" style={{padding: `15px 20px`}} value={selectedDepartureDateFrom} onChange={DEPDateChangeHandlerFrom} />
+            <input type="date" style={{padding: `15px 20px`}} value={selectedDepartureDateTo} onChange={DEPDateChangeHandlerTo} />
+            </div>
+
+            <Button onClick={flightSearchHandler} variant="contained" style={{padding: `15px 50px`}}>Search</Button>
+
+        
     </Card>)
 }
 
