d2ecb872aaf242e7343bbcdb66e72465b5d813d5
goskam <matusiak.goska@gmail.com>
2023-08-31 11:41:57 +0200

    Gosia's commit

--- src/components/Flights/FlightSearch/FlightSearchForm.js
+++ src/components/Flights/FlightSearch/FlightSearchForm.js
@@ -86,6 +86,11 @@ const FlightSearchForm = (props) => {
         
     }
 
+
+    const gosiasFunction = () => {
+        console.log("gosia")
+    }
+    
     return (<Card>
         <form onSubmit={flightSearchHandler} display="flex">        
             <TextField id="fly_from" label="From" variant="outlined" value={enteredDepartureAirport} onChange={DEPAirportChangeHandler}/>
